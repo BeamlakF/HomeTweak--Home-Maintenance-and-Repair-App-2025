7fb2667e78baf3db428d9d619c003bc2571271d4
Thitna Damtew <thitnadamtew@gmail.com>
2025-05-11 07:06:14 +0300

    Update CategoryService.kt

--- app/src/main/java/com/example/myapplication/data/remote/CategoryService.kt
+++ app/src/main/java/com/example/myapplication/data/remote/CategoryService.kt
@@ -3,13 +3,9 @@ package com.example.myapplication.data.remote
 import com.example.myapplication.data.model.Category
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface CategoryService {
 
     @GET("categories")
-    fun getCategories(): Call<List<Category>>
-
-    @GET("categories/{id}")
-    fun getCategoryById(@Path("id") categoryId: Int): Call<Category>
+    fun getAllCategories(): Call<List<Category>>
 }
