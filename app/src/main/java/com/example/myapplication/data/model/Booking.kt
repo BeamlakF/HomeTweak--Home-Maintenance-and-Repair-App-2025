807ed05ff6d2ee38f1e85e398c495060cf86a0f0
Beamlak F <beamlakfekadu562@gmail.com>
2025-05-13 00:59:43 -0700

    model

--- app/src/main/java/com/example/myapplication/data/model/Booking.kt
+++ app/src/main/java/com/example/myapplication/data/model/Booking.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.data.model
 data class Booking(
     val id: Int,
     val userId: Int,
-    val providerId: Int,
+//    val providerId: Int,
     val serviceType: String,
     val serviceDate: String,
     val bookingDate: String,
